be853198ebbef77d3020bb6387e23eab81eb1892
TiBappy <tibappy95@gmail.com>
2026-03-17 14:06:29 +0600

    Initialized the website called pricing Card

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return <>
+    <h1 className="text-6xl text-red-400">My Name is Bappy Mridha</h1>
+    <button className="btn btn-success">Success</button>
+  </>;
+}
+
+export default App;
